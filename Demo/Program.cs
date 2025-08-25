d860c5399637533341a3d2a4d2013a3666ce29a7
Aliaa Tarek <Aliaa Tarek@LAPTOP-LJC895S6>
2025-08-25 12:42:52 +0300

    Generic - Dictionary

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using Demo.HashTable;
+﻿using Demo.Dictionary;
+using Demo.HashTable;
 using System.Collections;
 
 namespace Demo
@@ -79,6 +80,111 @@ namespace Demo
 
 			#endregion
 
+			#region Generic - Dictionary  
+
+			#region Example 01
+			//Dictionary<string, int> PhoneNote = new Dictionary<string, int>
+			//{
+			//	{ "Mona", 111 },
+			//	{ "Ali", 222 },
+			//	{ "Salma", 333 }
+			//};
+
+
+			//KeyValuePair<string, int>[] array =
+			//[
+			//	new("Mona" , 111),
+			//	new("Ahmed" , 222)
+			//];
+
+			//PhoneNote = new Dictionary<string, int>(array, new Dictionary.StringEqualityComparer());
+
+
+			//PhoneNote.Add("mona", 123); // ArgumentException  
+
+			//foreach (var item in PhoneNote)
+			//	Console.WriteLine(item);
+			//// [Mona, 111]
+			//// [Ahmed, 222]
+
+
+			#region Indexer 
+
+			//// Using Indexer To Set Value 
+			//PhoneNote["Mona"] = 100;
+
+			//// Using Indexer To Get Value
+			//Console.WriteLine(PhoneNote["Mona"]);
+			////Console.WriteLine(PhoneNote["Omar"]); // KeyNotFoundException -> Unsafe 
+
+			//bool Result = PhoneNote.TryGetValue("Omar", out int number);
+			//Console.WriteLine(Result);// False
+			//Console.WriteLine(number);// 0
+
+
+			//// Using Indexer To Add New Element 
+			//PhoneNote["Omar"] = 1000;  
+			//// If Found => Reset
+			//// If Not => Add
+
+			#endregion
+
+			#region Add Element 
+
+			//PhoneNote.Add("Mohamed", 1000);
+			////PhoneNote.Add("Mohamed", 1000); // ArgumentException
+
+			//// Safe Way 
+			//if (!PhoneNote.ContainsKey("Mohamed"))
+			//	PhoneNote.Add("Mohamed", 1000);
+
+			//bool Result = PhoneNote.TryAdd("Mohamed", 1000);
+			//Console.WriteLine(Result); // False 
+
+
+			#endregion
+
+			#region Iteration on Dictionary
+			//foreach (string item in PhoneNote.Keys)
+			//	Console.WriteLine(item);
+
+			//foreach (int item in PhoneNote.Values)
+			//	Console.WriteLine(item);
+
+			//foreach (KeyValuePair<string , int> item in PhoneNote)
+			//	Console.WriteLine(item);
+			//// [Mona, 111]
+			//// [Ali, 222]
+			//// [Salma, 333] 
+			#endregion
+
+			#endregion
+
+			#region Example 02 - Employee
+
+			//Employee employee01 = new Employee(10, "Omar", 10000);
+			//Employee employee02 = new Employee(20, "Salma", 5000);
+			//Employee employee03 = new Employee(30, "Ahmed", 9000);
+
+			//Dictionary<Employee, string?> employees = new(new EmployeeIdEqualityComparer())
+			//{
+			//	[employee01] = "1st",
+			//	[employee02] = "2nd",
+			//	[employee03] = "3rd"
+			//};
+			//Employee employee04 = new Employee(30, "Salma", 9000);
+
+			//employees.Add(employee04, "4th"); // ArgumentException
+
+			//foreach (var item in employees)
+			//	Console.WriteLine(item);
+			//// [Id = 10 , name = Omar , Salary = 10000, 1st]
+			//// [Id = 20 , name = Salma , Salary = 5000, 2nd]
+			//// [Id = 30 , name = Ahmed , Salary = 9000, 3rd]
+
+			#endregion
+
+			#endregion
 		}
 	}
 }
